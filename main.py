9a4683724ff71c25c2b5bcf0552f49889f92ebf6
PollyGit <63475486+PollyGit@users.noreply.github.com>
2024-09-08 21:15:11 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import pygame  #импортируем библиотеку
 
 pygame.init()  #инициализируем библиотеку
 
+# создание констант в ветке part-constant-code
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+#переменная, где создается экран
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#задаем название нашего созданного окна
+pygame.display.set_caption('Игра ТИР')
+#создание иконки
+icon = pygame.image.load('')
+
+
 #создание игрового цикла, в данном случае while
 running = True
 while True:
