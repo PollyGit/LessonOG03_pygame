0ab82ed1c30a03132c8e62047dba7201497cd014
PollyGit <63475486+PollyGit@users.noreply.github.com>
2024-09-08 22:54:15 +0300

    add flying dove

--- main.py
+++ main.py
@@ -36,6 +36,18 @@ score = 0
 last_move_time = pygame.time.get_ticks()
 move_interval = 2000  # Интервал перемещения в миллисекундах
 
+# Переменные для нового объекта
+moving_object_image = pygame.image.load('image/dove_tir80.png')
+moving_object_image_width = 50
+moving_object_image_height = 50
+#moving_object_image = pygame.Surface((30, 30))
+#moving_object_image.fill((255, 0, 0))
+moving_object_x = None
+moving_object_y = random.randint(0, SCREEN_HEIGHT - 50)
+moving_object_speed = 1
+last_object_spawn_time = pygame.time.get_ticks()
+spawn_interval = random.randint(2000, 5000)  # 10-20 секунд
+
 
 #создание игрового цикла, в данном случае while
 running = True
@@ -64,6 +76,26 @@ while running:
         target_x = random.randint(0, SCREEN_WIDTH - target_width)
         target_y = random.randint(0, SCREEN_HEIGHT - target_height)
         last_move_time = current_time
+    # Появление и движение нового объекта
+    if moving_object_x is None and current_time - last_object_spawn_time > spawn_interval:
+        # Определяем, с какой стороны появится объект
+        if random.choice([True, False]):
+            moving_object_x = 0  # Появляется слева
+            moving_object_speed = 1
+        else:
+            moving_object_x = SCREEN_WIDTH - 50  # Появляется справа
+            moving_object_speed = -1
+        moving_object_y = random.randint(0, SCREEN_HEIGHT - 50)
+        last_object_spawn_time = current_time
+        spawn_interval = random.randint(2000, 5000)  # Обновляем интервал появления
+
+    if moving_object_x is not None:
+        moving_object_x += moving_object_speed
+        screen.blit(moving_object_image, (moving_object_x, moving_object_y))
+        # Проверяем, не вышел ли объект за пределы экрана
+        if moving_object_x < -50 or moving_object_x > SCREEN_WIDTH:
+            moving_object_x = None  # Удаляем объект
+
     #указываем отрисовку объекта target_image и его координаты
     screen.blit(target_image, (target_x, target_y))
     # Отрисовка очков
